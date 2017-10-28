6277a9b005b5fd22d901e7e9eead3bb5731d84c9
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2017-10-28 21:39:26 +0300

    MVC project for exercise

---
+++ MVC/ExerciseFromVideos/ExerciseFromVideos/App_Start/RouteConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ExerciseFromVideos
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.academy/{*pathInfo}"); // ne obrabotva link sys academy v nego
+
+            routes.LowercaseUrls = true;
+
+            routes.MapRoute(
+                name: "ProbenTochen",
+                url: "tochenadress/trqbvadavlezne",
+                defaults: new { controller = "Users", action = "StringMethod", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "ProbenControllerVKraq",
+                url: "tochenadress/trqbvadavlezne/{controller}",
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "ProbenControllerVSredata",
+                url: "tochenadress/trqbvadavlezne/{controller}/neshto",
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "ProbenControllerVSredataIVNachaloto",
+                url: "{action}/tochenadress/trqbvadavlezne/{controller}/neshto",
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "EnteredDate",
+                url: "{day}/{month}/{year}",
+                defaults: new { controller = "Users", action = "DateTime" });
+
+            routes.MapRoute(
+                name: "ActionResultStringMethod",
+                url: "{controller}/{action}/{firstNumber}/{secondNumber}/{name}",
+                defaults: new { controller = "Home", action = "Index", firstNumber = UrlParameter.Optional, secondNumber = UrlParameter.Optional, name = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ MVC/ExerciseFromVideos/ExerciseFromVideos/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExerciseFromVideos.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return Content("Page about the users.");
+        }
+
+        public string StringMethod(int? firstNumber= 0,int secondNumber = 0,string name="")
+        {
+            return $"{name} is {firstNumber+secondNumber} years old.";
+        }
+
+        public ActionResult DateTime(int? day, int? month, int? year)
+        {
+            return Content($"The date you entered is {day}.{month}.{year}");
+        }
+    }
+}
