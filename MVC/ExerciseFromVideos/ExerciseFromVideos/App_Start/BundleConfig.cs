b9d45cbbf9f8e436ad35f5d589720e670a265fd0
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2017-11-02 13:14:09 +0200

    Ajax in MVC

--- MVC/ExerciseFromVideos/ExerciseFromVideos/App_Start/BundleConfig.cs
+++ MVC/ExerciseFromVideos/ExerciseFromVideos/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace ExerciseFromVideos
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,8 @@ namespace ExerciseFromVideos
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
---
+++ MVC/ExerciseFromVideos/ExerciseFromVideos/Controllers/AjaxDemoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExerciseFromVideos.Controllers
+{
+    public class AjaxDemoController : Controller
+    {
+        // GET: AjaxDemo
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult AjaxTest()
+        {
+            return this.View();
+        }
+
+        public ActionResult ServerTime()
+        {
+            Thread.Sleep(1000);
+            return this.Content(DateTime.Now.ToLongDateString());
+        }
+
+        public ActionResult Search(string query) //trqbva da se kazva kato polenceto vyv formichkata
+        {
+            List<string> texts = new List<string>()
+            {
+                "some text",
+                "some other text",
+                "Alpha Rocks"
+            };
+            var result = texts.Where(t => t.ToLower().Contains(query.ToLower())).ToList();
+
+            return this.PartialView("_SearchResults", result);
+        }
+
+        public JsonResult AllBooks()
+        {
+            var books = new List<string>() { "book1", "book2", "book3" };
+            return this.Json(books, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
