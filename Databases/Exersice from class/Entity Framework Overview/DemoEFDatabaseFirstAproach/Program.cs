216d2a846b25b6274c6eef181c464cd6799847aa
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2018-03-22 01:39:13 +0200

    Telerik Academy Alpha - Databases - Entity Framework Overview video - exercise from class - Database first approach

---
+++ Databases/Exersice from class/Entity Framework Overview/DemoEFDatabaseFirstAproach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEFDatabaseFirstAproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelerikAcademyEntities db = new TelerikAcademyEntities();
+            db.Towns.Add(new Town() { Name = "Dimitrovgrad" });
+            db.SaveChanges();
+        }
+    }
+}
