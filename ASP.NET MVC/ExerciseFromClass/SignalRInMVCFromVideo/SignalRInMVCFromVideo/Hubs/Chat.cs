7cf084cffcf09d46a172178c05cbd980ea5b7480
IliyaDamyanov <i.s.damyanov@gmail.com>
2018-04-25 16:36:37 +0300

    Telerik Academy Alpha - MVC - SignalRInMVCFromVideo - building chat app . 1:33:56 watched from the video

---
+++ ASP.NET MVC/ExerciseFromClass/SignalRInMVCFromVideo/SignalRInMVCFromVideo/Hubs/Chat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRInMVCFromVideo.Hubs
+{
+    public class Chat :Hub
+    {
+        public void SendMessage(string message)
+        {
+            string finalMessage = $"Message from {this.Context.ConnectionId}: {message}";
+            this.Clients.All.newMessage(finalMessage);
+        }
+    }
+}
