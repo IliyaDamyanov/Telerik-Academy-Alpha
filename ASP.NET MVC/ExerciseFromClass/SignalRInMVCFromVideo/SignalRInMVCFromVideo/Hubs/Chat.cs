fcff1a794f1f6cfa5bdcd90a1a48e4ac5946fff1
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2018-04-29 17:40:02 +0300

    Telerik Academy Alpha - MVC - SignalRInMVCFromVideo - ready

--- ASP.NET MVC/ExerciseFromClass/SignalRInMVCFromVideo/SignalRInMVCFromVideo/Hubs/Chat.cs
+++ ASP.NET MVC/ExerciseFromClass/SignalRInMVCFromVideo/SignalRInMVCFromVideo/Hubs/Chat.cs
@@ -2,16 +2,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace SignalRInMVCFromVideo.Hubs
 {
-    public class Chat :Hub
+    public class Chat : Hub
     {
         public void SendMessage(string message)
         {
-            string finalMessage = $"Message from {this.Context.ConnectionId}: {message}";
+            string finalMessage = $"Message from {this.Context.User.Identity.Name}: {message}"; // taka ako e lognat samo shte dostypi
+            //string finalMessage = $"Message from {this.Context.ConnectionId}: {message}";
             this.Clients.All.newMessage(finalMessage);
+        }
+
+        public void JoinRoom(string roomName)
+        {
+            //add to database this.Context.User.Name with roomname
+            this.Groups.Add(this.Context.ConnectionId, roomName);
+        }
+
+        public override Task OnConnected()
+        {
+            if (this.Context.User.Identity.Name=="") //provarqvame dali e edi koi si
+            {
+                this.Groups.Add(this.Context.ConnectionId, "get from db"); //nqkvo ime i tuka
+            }
+            return base.OnConnected();
+        }
+
+        public void SendMessageToRoom(string message, string roomName)
+        {
+            string finalMessage = $"Message from {this.Context.User.Identity.Name}: {message}";
+            //string finalMessage = $"Message from {this.Context.ConnectionId}: {message}";
+            this.Clients.Group(roomName).newMessageToRoom(finalMessage);
         }
     }
 }
